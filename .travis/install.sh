e3b22efe97dd701310f997e880acf66fc04679ba
aganezov <aganezov@gwu.edu>
2016-07-22 12:19:07 -0400

    trying to fix mac os python

---
+++ .travis/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    brew update
+    brew install mummer
+    case "${PYVER}" in
+        py34)
+            brew install python3.4
+        ;;
+        py35)
+            brew install python3.5
+        ;;
+    esac
+else
+    apt-get update
+    apt-get install mummer
+fi
