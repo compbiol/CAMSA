3fb5a86a039b7a0e3620140e0b03dcb59bdf262e
aganezov <aganezov@gwu.edu>
2016-11-17 11:55:32 -0500

    initial setup for the reference analysis with main function implementation start

--- camsa/core/reference_analysis.py
+++ camsa/core/reference_analysis.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+import networkx as nx
+from camsa.core.data_structures import ScaffoldAssemblyGraph
+
+
+def analyze_and_update_assembly_points_based_on_reference(assembly, ref_assembly):
+    reference_graph = ScaffoldAssemblyGraph.from_assembly_points(assembly_points=ref_assembly.aps, reference=True)
+    ref_all_pairs_paths = nx.all_pairs_shortest_path(G=reference_graph.graph)
