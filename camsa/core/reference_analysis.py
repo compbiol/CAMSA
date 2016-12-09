576e7c4ba6ec7018c3b66d26e1c19ce0f1bb962f
aganezov <aganezov@gwu.edu>
2016-12-09 11:41:38 -0500

    continuation of the setup for the reference analysis in CAMSA

--- camsa/core/reference_analysis.py
+++ camsa/core/reference_analysis.py
@@ -6,3 +6,10 @@ from camsa.core.data_structures import ScaffoldAssemblyGraph
 def analyze_and_update_assembly_points_based_on_reference(assembly, ref_assembly):
     reference_graph = ScaffoldAssemblyGraph.from_assembly_points(assembly_points=ref_assembly.aps, reference=True)
     ref_all_pairs_paths = nx.all_pairs_shortest_path(G=reference_graph.graph)
+
+
+def has_unoriented_assembly_points(assembly):
+    return not all(map(lambda ap: ap.is_oriented, assembly.aps))
+
+
+
