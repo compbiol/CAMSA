6c59408bd3c43d4605445daaad020ec1878a589e
aganezov <aganezov@gwu.edu>
2016-12-20 20:30:28 -0500

    ragout sequence entry parsing implementation w.r.t coords file

--- camsa/utils/ragout/data_structures.py
+++ camsa/utils/ragout/data_structures.py
@@ -17,5 +17,21 @@ class RagoutSequence(Sequence):
         self.ragout_id = ragout_id
 
 
+def parse_ragout_seq_entry(entry_as_a_string, seq_id_column=0, seq_length_column=1, seq_description_column=2, delimiter="\t"):
+    """
+        Seq_id	Size	Description
+        27	11234	Anc0.Anc0refChr7043
+    """
+    data = entry_as_a_string.split(delimiter)
+    seq_id = int(float(data[seq_id_column]))
+    seq_length = int(float(data[seq_length_column]))
+    seq_name = data[seq_description_column]
+    return RagoutSequence(name=seq_name, ragout_id=seq_id, length=seq_length)
+
+
 def parse_ragout_block_entry(entry_as_a_string, sequences_by_ids=None):
+    pass
+
+
+def parse_block_id_string(data_string):
     pass
