9e3cb4122e8554b3b6a884701fc67f4b450d30c8
aganezov <aganezov@gwu.edu>
2016-12-25 16:37:49 -0500

    setting up logger ensuring the reference genome is correct for the convertion

--- camsa/utils/ragout/ragout_coords2fasta.py
+++ camsa/utils/ragout/ragout_coords2fasta.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import datetime
+import logging
 import os
 import sys
 
 import configargparse
-import logging
 
-from utils.ragout.shared import filter_blocks_by_good_genomes, filter_blocks_by_bad_genomes
+from utils.ragout.shared import filter_blocks_by_good_genomes, filter_blocks_by_bad_genomes, get_all_genomes_from_blocks
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
@@ -34,6 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--filter_duplications", type=bool, action="store_true", dest="filter_duplications", default=False)
     parser.add_argument("--good-genomes", type=str, default="", help="A coma separated list of genome names, to be processed and conversed.\nDEFAULT: \"\" (i.e., all genomes are good)")
     parser.add_argument("--bad-genomes", type=str, default="", help="A coma separated list of genome names, to be excluded from processing and conversion.\nDEFAULT: \"\" (i.e., no genomes are bad)")
+    parser.add_argument("--silent-block-skip", action="store_true", default=False, dest="silent_block_skip", type=bool)
     # parser.add_argument("fasta", nargs="+")
     parser.add_argument("--c-logging-level", dest="logging_level", default=logging.INFO, type=int,
                         choices=[logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL],
@@ -42,7 +44,20 @@ if __name__ == "__main__":
                         help="Format string for python logger.")
     args = parser.parse_args()
 
+    start_time = datetime.datetime.now()
+
+    logger = logging.getLogger("CAMSA.utils.ragout_coords2fasta")
+    ch = logging.StreamHandler()
+    ch.setLevel(args.c_logging_level)
+    logger.setLevel(args.c_logging_level)
+    logger.addHandler(ch)
+    logger.info(full_description)
+    logger.info(parser.format_values())
+    ch.setFormatter(logging.Formatter(args.c_logging_formatter_entry))
+    logger.info("Starting the converting process")
+
     sequences_by_ids, blocks_by_ids = ragout_io.read_from_file(path=args.ragout_coords, silent_fail=False, delimiter="\t")
+    all_genomes = get_all_genomes_from_blocks(blocks_as_ids=blocks_by_ids)
     if args.good_genomes != "":
         good_genomes = set(args.good_genomes.split(","))
         filter_blocks_by_good_genomes(blocks_by_ids=blocks_by_ids, good_genomes=good_genomes)
@@ -50,8 +65,16 @@ if __name__ == "__main__":
         bad_genomes = set(args.bad_genomes.split(","))
         filter_blocks_by_bad_genomes(blocks_by_ids=blocks_by_ids, bad_genomes=bad_genomes)
     if args.filter_indels:
-        filter_indels(blocks_by_ids=blocks_by_ids)
+        filter_indels(blocks_by_ids=blocks_by_ids, all_genomes_as_set=all_genomes)
     if args.filter_duplications:
         filter_duplications(blocks_by_ids=blocks_by_ids)
-
-    a = 5
+    all_filtered_genomes = get_all_genomes_from_blocks(blocks_as_ids=blocks_by_ids)
+    if args.ref_genome == "":
+        logger.info("Reference genome was not specified")
+        args.ref_genome = all_filtered_genomes.pop()
+        logger.info("Setting reference genome to {ref_genome}".format(ref_genome=args.ref_genome))
+    else:
+        if args.ref_genome not in all_filtered_genomes:
+            logger.critical("Reference genome {ref_genome} was not present in all filtered genome {filtered_genomes}"
+                            "".format(ref_genome=args.ref_genome, filtered_genomes=",".join(all_filtered_genomes)))
+            exit(1)
