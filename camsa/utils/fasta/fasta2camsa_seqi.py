acd12a03bbc711ba00078a2f1c75e81f37c45914
aganezov <aganezov@gwu.edu>
2017-01-14 17:54:42 -0500

    revamping *camsa_length into *2camsa_seqi script with a more expanded ability of more information extracting, in concordance with new meta Sequence data structure

--- camsa/utils/fasta/fasta2camsa_seqi.py
+++ camsa/utils/fasta/fasta2camsa_seqi.py
@@ -9,10 +9,13 @@ import sys
 import configargparse
 from Bio import SeqIO
 
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
 import camsa
+from camsa.core.data_structures import Sequence
+from camsa.core.io import write_seqi
 
 if __name__ == "__main__":
     full_description = camsa.full_description_template.format(
@@ -23,10 +26,14 @@ if __name__ == "__main__":
         information="For more information refer to {docs}".format(docs=camsa.CAMSA_DOCS_URL),
         contact=camsa.CONTACT)
     full_description = "=" * 80 + "\n" + full_description + "=" * 80 + "\n"
-    parser = configargparse.ArgParser(description=full_description, formatter_class=configargparse.RawTextHelpFormatter)
+    parser = configargparse.ArgParser(description=full_description, formatter_class=configargparse.RawTextHelpFormatter,
+                                      default_config_files=[os.path.join(camsa.root_dir, "utils", "fasta", "fasta2camsa_seqi.ini"),
+                                                            os.path.join(camsa.root_dir, "logging.ini")])
+
     parser.add_argument("--version", action="version", version=camsa.VERSION)
     parser.add_argument("-o", "--output-file", metavar="OUTPUT", dest="output", type=configargparse.FileType("wt"), default=sys.stdout,
                         help="A file to which the CAMSA readable fragments lengths would be written. Standard extension is \".camsa.lengths\".\nDEFAULT: stdout")
+    parser.add_argument("--o-format", type=str, help="")
     parser.add_argument("contigs", nargs="+", metavar="CONTIGS", type=configargparse.FileType("rt"), default=sys.stdin,
                         help="A list of input *.fasta files with contigs.\nDEFAULT: stdin")
     parser.add_argument("--c-logging-level", dest="logging_level", default=logging.INFO, type=int,
@@ -39,7 +46,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     start_time = datetime.datetime.now()
 
-    logger = logging.getLogger("CAMSA.utils.fasta2camsa_lengths")
+    logger = logging.getLogger("CAMSA.utils.fasta2camsa_seqi")
     ch = logging.StreamHandler()
     ch.setLevel(args.logging_level)
     logger.setLevel(args.logging_level)
@@ -54,14 +61,11 @@ if __name__ == "__main__":
         logger.info("Processing file: \"{file_name}\"".format(file_name=f))
         cnt = 0
         for record in SeqIO.parse(f, "fasta"):
-            entries[record.id] = len(record.seq)
+            seq = Sequence(name=record.id, parent_seq_id=record.id, start=0, end=len(record.seq), strand="+")
+            entries[record.id] = seq
             cnt += 1
         logger.info("Processed {cnt} fasta records".format(cnt=cnt))
-    writer = csv.writer(args.output, delimiter=args.o_delimiter)
-    logger.info("Writing the CAMSA lengths output")
-    writer.writerow(["seq_id", "seq_length"])
-    for key in sorted(entries.keys()):
-        writer.writerow([key, entries[key]])
+    write_seqi(sequences=entries.values(), destination=args.output, output_setup=args.o_format, delimiter=args.o_delimiter)
     logger.info("All done!")
     end_time = datetime.datetime.now()
     logger.info("Elapsed time: {el_time}".format(el_time=str(end_time - start_time)))
