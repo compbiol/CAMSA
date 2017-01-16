31f3127d094e73e8195fc889f8ccefba0313843e
aganezov <aganezov@gwu.edu>
2017-01-15 19:02:27 -0500

    updated camsa_points2fasta converter script (while preserving the reverse compatibility):
    * added support for synteny bocks, when fasta source is not exact, but rather a parent sequence with start/end/strand attributes for the block
    * added gap filling support, when assembly point is between two blocks on the same parent sequence. Configurable parameters for gap filling (thresholds for distance difference in percentages of gap size/actual bp length).
    * added extension of outermost fragments in the constructed scaffolds, when outermost blocks have a parent sequence, that has more sequence to the side of used block

--- camsa/utils/fasta/camsa_points2fasta.py
+++ camsa/utils/fasta/camsa_points2fasta.py
@@ -23,11 +23,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
 
 import camsa
 from camsa.core.io import read_pairs, read_seqi_from_input_sources
-from camsa.core.data_structures import get_scaffold_edges
+from camsa.core.data_structures import get_scaffold_edges, Sequence
 
 
 def get_scaffold_name_from_vertex(v):
     return v[:-1]
+
+
+def reverse_or(orientaiton):
+    return "+" if orientaiton == "-" else "-"
+
+
+def reverse_ap(f1, f1_or, f2, f2_or):
+    return f2, reverse_or(orientaiton=f2_or), f1, reverse_or(orientaiton=f1_or)
 
 
 def get_assembly_edge(graph):
@@ -76,7 +84,9 @@ if __name__ == "__main__":
                         help="A stream of CAMSA formatted assembly points, representing a scaffold assembly, that is converted into FASTA formatted sequences")
     parser.add_argument("--seqi", default="", type=str)
     parser.add_argument("--seqi-delimiter", default="\t", type=str)
-    parser.add_argument("--fill-gaps", default=None)
+    parser.add_argument("--fill-gaps", action="store_true")
+    parser.add_argument("--extend-ends", action="store_true")
+    parser.add_argument("--fill-gaps-unknown", action="store_true")
     parser.add_argument("--gap-diff-threshold-per", default=10.0, type=float)
     parser.add_argument("--gap-diff-threshold-bp", default=1000, type=float)
     parser.add_argument("--allow-singletons", action="store_true", dest="allow_singletons", default=False,
@@ -178,35 +188,100 @@ if __name__ == "__main__":
         with open(args.seqi, "rt") as source:
             read_seqi_from_input_sources(source=source, delimiter=args.seqi_delimiter, destination=sequences_by_ids)
 
+    participating_sequences_by_ids = {}
+    # for every fragment name that is not present as meta-sequence info a dumb one is created
+    for fragment_aps in fragments:
+        for f1, f1_or, f2, f2_or, gap_size in fragment_aps:
+            if f1 not in sequences_by_ids:
+                sequences_by_ids[f1] = Sequence(name=f1)
+            if f2 not in sequences_by_ids:
+                sequences_by_ids[f2] = Sequence(name=f2)
+            participating_sequences_by_ids[f1] = sequences_by_ids[f1]
+            participating_sequences_by_ids[f2] = sequences_by_ids[f2]
+
+    meta_seqs_by_parent_ids = defaultdict(list)
+    for seq in participating_sequences_by_ids.values():
+        meta_seqs_by_parent_ids[seq.parent_seq_id].append(seq)
+
+    for parent_seq_id in list(meta_seqs_by_parent_ids.keys()):
+        meta_seqs_by_parent_ids[parent_seq_id] = sorted(meta_seqs_by_parent_ids[parent_seq_id], key=lambda seq: (seq.start, seq.end))
+
     logger.debug("Processed {cnt} records from fasta file \"{file_name}\"".format(cnt=s_cnt, file_name=args.fasta))
     logger.info("Total number of contig/scaffold sequences is {seq_cnt}".format(seq_cnt=len(frag_fasta_by_id)))
 
-    for fragment_aps in fragments:
-        for f1, f1_or, f2, f2_or, gap_size in fragment_aps:
-            if f1 not in frag_fasta_by_id or f2 not in frag_fasta_by_id:
-                logging.critical("Fragment {f1} or {f2} which is present assembly points is not present in supplied fasta file. Exiting.".format(f1=f1, f2=f2))
-                exit(1)
+    for sequence in participating_sequences_by_ids.values():
+        if sequence.parent_seq_id not in frag_fasta_by_id:
+            logging.critical("Fragment {sequence} (parent for {block_name}) is not present in supplied fasta file. Exiting.".format(sequence=sequence.parent_seq_id, block_name=sequence.name))
+            exit(1)
 
     used_fragments = set()
     logger.info("Outputting new scaffolds. Data is written to {file_name}".format(file_name=args.output))
     for s_cnt, fragment_aps in enumerate(fragments):
         current = Seq("")
         for f_cnt, (f1, f1_or, f2, f2_or, gap_size) in enumerate(fragment_aps):
-            used_fragments.add(f1)
-            used_fragments.add(f2)
-            if f1_or == "+":
-                current += frag_fasta_by_id[f1].seq
+            meta_seq1 = participating_sequences_by_ids[f1]
+            meta_seq2 = participating_sequences_by_ids[f2]
+
+            seq1 = frag_fasta_by_id[meta_seq1.parent_seq_id].seq[meta_seq1.start:meta_seq1.end]
+            seq1 = seq1 if meta_seq1.strand == "+" else seq1.reverse_complement()
+            seq1 = seq1 if f1_or == "+" else seq1.reverse_complement()
+
+            if f_cnt == 0 and args.extend_ends:
+                if meta_seq1.name == meta_seqs_by_parent_ids[meta_seq1.parent_seq_id][0].name \
+                        and meta_seq1.strand == f1_or and meta_seq1.start > 0:
+                    extension = frag_fasta_by_id[meta_seq1.parent_seq_id][:meta_seq1.start].seq
+                    current += extension
+                if meta_seq1.name == meta_seqs_by_parent_ids[meta_seq1.parent_seq_id][-1].name \
+                        and meta_seq1.strand != f1_or:
+                    extension = frag_fasta_by_id[meta_seq1.parent_seq_id][meta_seq1.end:].seq.reverse_complement()
+                    current += extension
+
+            current += seq1
+
+            if args.fill_gaps:
+                if meta_seq1.parent_seq_id == meta_seq2.parent_seq_id:
+                    if meta_seq1.end < meta_seq2.start and f1_or == meta_seq1.strand and f2_or == meta_seq2.strand:
+                        gap = frag_fasta_by_id[meta_seq1.parent_seq_id][meta_seq1.end:meta_seq2.start].seq
+                    elif meta_seq2.end < meta_seq1.start and f1_or == reverse_or(orientaiton=meta_seq1.strand) and f2_or == reverse_or(orientaiton=meta_seq2.strand):
+                        gap = frag_fasta_by_id[meta_seq1.parent_seq_id][meta_seq2.end:meta_seq1.start].seq.reverse_complement()
+                    else:
+                        gap = Seq("")
+                    gap_length = len(gap)
+                    ap_gap_size = gap_size if isinstance(gap_size, numbers.Number) else "?"
+                    if ap_gap_size == "?":
+                        if args.fill_gaps_unknown:
+                            current += gap
+                    else:
+                        diff = abs(ap_gap_size - gap_length)
+                        if diff * 100.0 / ap_gap_size < args.gap_diff_threshold_per:
+                            current += gap
+                        elif diff < args.gap_diff_threshold_bp:
+                            current += gap
             else:
-                current += frag_fasta_by_id[f1].reverse_complement().seq
-            sep_length = gap_size if isinstance(gap_size, numbers.Number) else args.c_sep_length
-            if sep_length <= 0:
-                sep_length = args.c_sep_length
-            current += Seq(args.c_sep * int(sep_length))
+                gap_length = gap_size if isinstance(gap_size, numbers.Number) else args.c_sep_length
+                if gap_length <= 0:
+                    gap_length = args.c_sep_length
+                gap = Seq(args.c_sep * int(gap_length))
+                current += gap
+
+            used_fragments.add(meta_seq1.parent_seq_id)
+            used_fragments.add(meta_seq2.parent_seq_id)
+
             if f_cnt == len(fragment_aps) - 1:
-                if f2_or == "+":
-                    current += frag_fasta_by_id[f2].seq
-                else:
-                    current += frag_fasta_by_id[f2].reverse_complement().seq
+                seq2 = frag_fasta_by_id[meta_seq2.parent_seq_id].seq[meta_seq2.start:meta_seq2.end]
+                seq2 = seq2 if meta_seq2.strand == "+" else seq2.reverse_complement()
+                seq2 = seq2 if f2_or == "+" else seq2.reverse_complement()
+                current += seq2
+                if args.extend_ends:
+                    if meta_seq2.name == meta_seqs_by_parent_ids[meta_seq2.parent_seq_id][0].name \
+                            and meta_seq2.strand != f2_or and meta_seq2.start > 0:
+                        extension = frag_fasta_by_id[meta_seq2.parent_seq_id].seq[:meta_seq2.start].reverse_complement()
+                        current += extension
+                    if meta_seq2.name == meta_seqs_by_parent_ids[meta_seq2.parent_seq_id][-1].name \
+                            and meta_seq2.strand == f2_or:
+                        extension = frag_fasta_by_id[meta_seq2.parent_seq_id].seq[meta_seq2.end:]
+                        current += extension
+
         name = args.scaffold_name_template.format(cnt=s_cnt)
         seq_record = SeqRecord(seq=current, id=name, description="")
         SeqIO.write(sequences=seq_record, handle=args.output, format="fasta")
