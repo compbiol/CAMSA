f51733963a6660b510ee51d62de52cc64bc6394e
aganezov <aganezov@gwu.edu>
2017-02-09 17:13:05 -0500

    refactoring of the ragout_coords2camsa_seqi script:
    * supports multi reference blocks formatting
    * removed useless --sbs flag

--- camsa/utils/ragout/ragout_coords2camsa_seqi.py
+++ camsa/utils/ragout/ragout_coords2camsa_seqi.py
@@ -1,14 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import csv
 import datetime
 import logging
 import os
 import sys
 
 import configargparse
-from Bio import SeqIO
-
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
@@ -37,14 +34,15 @@ if __name__ == "__main__":
                         help="A file to which the CAMSA readable fragments lengths would be written. Standard extension is \".camsa.lengths\".\nDEFAULT: stdout")
     parser.add_argument("--o-format", type=str, help="")
     parser.add_argument("ragout_coords", type=str, help="A path to ragout coords file")
-    parser.add_argument("--ref-genomes", type=str, default="")
+    parser.add_argument("--genomes-order", type=str, default="")
     parser.add_argument("--ann-genomes", type=str, default="")
     parser.add_argument("--ann-delimiter", type=str, default=";")
     parser.add_argument("--filter-indels", action="store_true", dest="filter_indels", default=False)
-    parser.add_argument("--filter-duplications", action="store_true", dest="filter_duplications", default=False)
+    parser.add_argument("--no-filter-indels", action="store_false", dest="filter_indels", default=False)
+    parser.add_argument("--filter-duplications", action="store_true", dest="filter_duplications", default=True)
+    parser.add_argument("--no-filter-duplications", action="store_false", dest="filter_duplications", default=True)
     parser.add_argument("--good-genomes", type=str, default="", help="A coma separated list of genome names, to be processed and conversed.\nDEFAULT: \"\" (i.e., all genomes are good)")
     parser.add_argument("--bad-genomes", type=str, default="", help="A coma separated list of genome names, to be excluded from processing and conversion.\nDEFAULT: \"\" (i.e., no genomes are bad)")
-    parser.add_argument("--sbs", action="store_true", default=False, dest="silent_block_skip")
     parser.add_argument("--c-logging-level", dest="logging_level", default=logging.INFO, type=int,
                         choices=[logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL],
                         help="Logging level for the converter.\nDEFAULT: {info}".format(info=logging.INFO))
@@ -78,56 +76,38 @@ if __name__ == "__main__":
     if args.filter_duplications:
         filter_duplications(blocks_by_ids=blocks_by_ids)
     all_filtered_genomes = get_all_genomes_from_blocks(blocks_as_ids=blocks_by_ids)
-
-    if args.ref_genomes == "":
-        logger.info("Reference genomes were not specified")
-        args.ref_genomes = sorted(all_filtered_genomes)
-        logger.info("Setting reference genomes to {ref_genome}".format(ref_genome=args.ref_genome))
+    if args.genomes_order != "":
+        genomes_order = args.genomes_order.split(",")
     else:
-        args.ref_genomes = args.ref_genomes.split(",")
-        for genome in args.ref_genomes:
-            if genome not in all_filtered_genomes:
-                logger.critical("Reference genome {ref_genome} was not present in all filtered genome {filtered_genomes}"
-                                "".format(ref_genome=args.genome, filtered_genomes=",".join(all_filtered_genomes)))
-                exit(1)
+        genomes_order = sorted(all_filtered_genomes)
+    for genome in genomes_order:
+        if genome not in all_filtered_genomes:
+            logger.critical("Genome \"{genome}\" was specified with teh --genomes-order flag but was not found in the set of filtered genomes \{{filtered_genomes}\}"
+                            "".format(genome=genome, filtered_genomes=",".join(sorted(all_filtered_genomes))))
+            exit(1)
     if args.ann_genomes == "":
-        args.ann_genomes = args.ref_genomes
+        args.ann_genomes = genomes_order
     else:
         args.ann_genomes = args.ann_genomes.split(",")
         for genome in args.ann_genomes:
-            if genome not in args.ref_genomes:
+            if genome not in genomes_order:
                 logger.critical("Annotation genomes {ann_genome} was not present in reference genomes {ref_genomes}"
-                                "".format(ann_genome=args.genome, ref_genomes=",".join(all_filtered_genomes)))
+                                "".format(ann_genome=args.genome, ref_genomes=",".join(sorted(all_filtered_genomes))))
                 exit(1)
     sequences = []
     for block_id in sorted(blocks_by_ids.keys()):
         blocks = blocks_by_ids[block_id]
-        blocks_by_ref_genomes = [block for block in blocks if block.parent_seq.genome_name in args.ref_genomes]
-        if len(blocks_by_ref_genomes) == 0:
-            if not args.silent_block_skip:
-                logger.critical("For blocks with id {block_id} not a single instance was present in the reference genome {ref_genome}. Flag \"--sbs\" was not set, and this event is thus critical."
-                                "".format(block_id=block_id, ref_genome=args.ref_genome))
-            else:
-                logger.warning("For blocks with id {block_id} not a single instance was present in the reference genome \"{ref_genome}\". Flag \"--sbs\" was set, thus silently ignoring this case."
-                               "".format(block_id=block_id, ref_genome=args.ref_genome))
-                continue
-        # if len(blocks_by_ref_genomes) > 1:
-        #     logger.warning("More than a single block with id {block_id} was found in the reference genome \"{ref_genome}\". Randomly choosing one such block (shall not be a problem, as they must be merely identical)"
-        #                    "".format(block_id=block_id, ref_genome=args.ref_genome))
         blocks = []
-        for ref_genome in args.ref_genomes:
-            for block in blocks_by_ref_genomes:
-                if block.parent_seq.genome_name == ref_genome:
-                    blocks.append(block)
+        for genome in genomes_order:
+            blocks.extend([block for block in blocks if block.parent_seq.genome_name == genome])
         annotations = []
         for ann_genome in args.ann_genomes:
-            for block in blocks_by_ref_genomes:
+            for block in blocks:
                 if block.parent_seq.genome_name == ann_genome:
                     annotations.append(block.annotation_name)
         annotation = args.ann_delimiter.join(annotations)
-        block = blocks[0]
-        sequences.append(Sequence(name=block.name, parent_seq_id=block.parent_seq.seq_name, start=block.start, end=block.end, strand=block.strand, annotation=annotation, seq_group_id=block.parent_seq.genome_name))
-
+        for block in blocks:
+            sequences.append(Sequence(name=block.name, parent_seq_id=block.parent_seq.seq_name, start=block.start, end=block.end, strand=block.strand, annotation=annotation, seq_group_id=block.parent_seq.genome_name))
     write_seqi(sequences=sequences, destination=args.output, output_setup=args.o_format, delimiter=args.o_delimiter)
     logger.info("All done!")
     end_time = datetime.datetime.now()
