80856ab4470f6fcac7922f74312a21b79be04458
aganezov <aganezov@cs.jhu.edu>
2018-09-28 16:27:11 -0400

    Travis update

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,43 +8,30 @@ if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
     fi
     bash ~/miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda info -a
+    conda create -n test-environment python=$PYTHON
+    source activate test-environment
     conda install -c bioconda mummer
     nucmer -h
-    brew update
-#    brew tap homebrew/science
-    brew install pyenv
-#    brew install mercurial
-#    brew install mummer
-#    brew install gcc
-    case "$PYTHON" in
-    "2.7")
-     pyenv install 2.7.14
-     pyenv global 2.7.14
-     ;;
-     "3.5")
-     pyenv install 3.5.4
-     pyenv global 3.5.4
-     ;;
-     "3.6")
-     pyenv install 3.6.3
-     pyenv global 3.6.3
-     ;;
-     "pypy.2")
-     pyenv install pypy-5.3
-     pyenv global pypy-5.3
-     ;;
-     "pypy.3")
-     pyenv install pypy3.3-5.2-alpha1
-     pyenv global pypy3.3-5.2-alpha1
-     ;;
-    esac
-    `pyenv which pip` install virtualenv
-    `pyenv which virtualenv` camsa-env
 fi
 
 if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
-    sudo apt-get update
-    sudo apt-get install mummer
-    pip install virtualenv
-    virtualenv camsa-env
+    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+      wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+    else
+      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    fi
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    # Useful for debugging any issues with conda
+    conda info -a
+    conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
+    source activate test-environment
+    conda install -c bioconda mummer
+    nucmer -h
 fi
