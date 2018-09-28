fa46f6226ecd3d20d0c64dcec69cc7d06a379191
aganezov <aganezov@cs.jhu.edu>
2018-09-28 16:57:10 -0400

    Travis CI fixing

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,11 +7,7 @@ if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
     fi
     bash ~/miniconda.sh -b -p $HOME/miniconda
-    if [[ "$PYTHON" = "2.7" ]]; then
-        export PATH="$HOME/miniconda2/bin:$PATH"
-    else
-        export PATH="$HOME/miniconda3/bin:$PATH"
-    fi
+    export PATH="$HOME/miniconda/bin:$PATH"
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
     conda info -a
