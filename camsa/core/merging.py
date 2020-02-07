7d98ac2569bcd8b3b5cea37482c08729d584b4f1
aganezov <aganezov@cs.jhu.edu>
2020-02-06 21:04:15 -0500

    PyPI classification typo correction

--- camsa/core/merging.py
+++ camsa/core/merging.py
@@ -129,7 +129,7 @@ def maximal_matching(assembly_points_by_sources, acyclic=True, min_cw=0.0):
     if acyclic:
         edges_to_delete = []
         for cc in networkx.connected_components(G=cover_graph):
-            cc = networkx.Graph(cc)
+            cc = cover_graph.subgraph(cc).copy()
             if cc.number_of_nodes() == cc.number_of_edges():
                 assembly_edges = filter(lambda entry: "weight" in entry[2], cc.edges(data=True))
                 edges_to_delete.append(min(assembly_edges, key=lambda entry: entry[2]["weight"]))
